5716490ca91b09b0ec4b1cbbf862edfc4a8e1a3d
Navaneet <11260095+nauaneed@users.noreply.github.com>
2021-12-20 22:40:42 +0530

    sjogreen works

--- pysph/examples/gas_dynamics/sjogreen.py
+++ pysph/examples/gas_dynamics/sjogreen.py
@@ -2,6 +2,7 @@
 """
 from pysph.examples.gas_dynamics.shocktube_setup import ShockTubeSetup
 from pysph.sph.scheme import ADKEScheme, GasDScheme, GSPHScheme, SchemeChooser
+from pysph.sph.gas_dynamics.cullen_dehnen.scheme import CullenDehnenScheme
 import numpy
 
 # Numerical constants
@@ -60,11 +61,18 @@ class SjoGreen(ShockTubeSetup):
         lng = numpy.zeros(1, dtype=float)
         consts = {'lng': lng}
 
-        return self.generate_particles(
+        f,b = self.generate_particles(
             xmin=self.xmin, xmax=self.xmax, dxl=self.dxl, dxr=self.dxr,
             m=self.dxl, pl=self.pl, pr=self.pr, h0=self.h0, bx=0.03,
             gamma1=gamma1, ul=self.ul, ur=self.ur, constants=consts
         )
+
+        self.scheme.setup_properties([f, b])
+        if self.options.scheme == 'cullendehnen':
+            # override Mh set by CullenDehnenScheme.setup_properties()
+            f.add_property('Mh', data=self.hdx*self.dxl*self.rhol)
+        return [f]
+
 
     def create_scheme(self):
         self.dt = dt
@@ -88,9 +96,41 @@ class SjoGreen(ShockTubeSetup):
             niter=40, tol=1e-6
         )
 
-        s = SchemeChooser(default='adke', adke=adke, mpm=mpm, gsph=gsph)
+        cullendehnen = CullenDehnenScheme(
+            fluids=['fluid'], solids=[], dim=dim, gamma=gamma,
+            l=0.1, alphamax=2.0, b=1.0, has_ghosts=True
+        )
+
+        s = SchemeChooser(default='adke', adke=adke, mpm=mpm, gsph=gsph,
+                          cullendehnen=cullendehnen)
         return s
 
+    def configure_scheme(self):
+        s = self.scheme
+        dxl = 0.5/self.nl
+        ratio = self.rhor/self.rhol
+        nr = ratio*self.nl
+        dxr = 0.5/self.nr
+        h0 = self.hdx * self.dxr
+        kernel_factor = self.options.hdx
+        if self.options.scheme == 'mpm':
+            s.configure(kernel_factor=kernel_factor)
+            s.configure_solver(dt=self.dt, tf=self.tf,
+                               adaptive_timestep=True, pfreq=50)
+        elif self.options.scheme == 'adke':
+            s.configure_solver(dt=self.dt, tf=self.tf,
+                               adaptive_timestep=False, pfreq=50)
+        elif self.options.scheme == 'gsph':
+            s.configure_solver(dt=self.dt, tf=self.tf,
+                               adaptive_timestep=False, pfreq=50)
+        elif self.options.scheme == 'crk':
+            s.configure_solver(dt=self.dt, tf=self.tf,
+                               adaptive_timestep=False, pfreq=1)
+        elif self.options.scheme == 'cullendehnen':
+            from pysph.base.kernels import Gaussian
+            s.configure_solver(dt=self.dt, tf=self.tf,
+                               adaptive_timestep=False, pfreq=50,
+                               kernel=Gaussian(dim=dim))
 
 if __name__ == '__main__':
     app = SjoGreen()
